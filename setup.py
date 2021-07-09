dc86b7de362c0efce215382c2780446ea00181ad
tinyCoder <65143775+tinyCodersDen@users.noreply.github.com>
2021-07-09 16:14:29 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,16 @@
 from distutils.core import setup
 setup(
   name = 'py-data-structure',         # How you named your package folder (MyLib)
-  packages = ['YOURPACKAGENAME'],   # Chose the same as "name"
+  packages = ['py-data-structure'],   # Chose the same as "name"
   version = '0.1',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'A package that allows you to implement C++ data structures in Python.',   # Give a short description about your library
   author = 'Vihan',                   # Type in your name
   author_email = 'vihan.raval1@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/tinyCodersDen/py-data-structure',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/tinyCodersDen/py-data-structure/archive/refs/tags/v_01.tar.gz',    # I explain this later on
   keywords = ['Data','structures','data structures','stacks','queues'],   # Keywords that define your package best
-  install_requires=[            # I get to this in a second
-          'validators',
-          'beautifulsoup4',
-      ],
+  install_requires=[],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
     'Intended Audience :: Developers',      # Define that your audience are developers
