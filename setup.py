8aee42b9beb52a7279f0a97f99ff4915202d047e
tinyCoder <65143775+tinyCodersDen@users.noreply.github.com>
2021-07-12 15:36:19 -0400

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,14 +1,18 @@
 from distutils.core import setup
+with open('README.md') as f:
+    long_description = f.read()
 setup(
   name = 'py-data-structure',         # How you named your package folder (MyLib)
   packages = ['py-data-structure'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
+  version = '0.1.1',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-  description = 'A package that allows you to implement C++ data structures in Python.',   # Give a short description about your library
+  description = 'A package that allows you to implement C++ data structures in Python.',
+  long_description=long_description,# Give a short description about your library
+  long_description_content_type='text/markdown',
   author = 'Vihan',                   # Type in your name
   author_email = 'vihan.raval1@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/tinyCodersDen/py-data-structure',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/tinyCodersDen/py-data-structure/archive/refs/tags/v_01.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/tinyCodersDen/py-data-structure/archive/refs/tags/v_0.1.1.tar.gz',    # I explain this later on
   keywords = ['Data','structures','data structures','stacks','queues'],   # Keywords that define your package best
   install_requires=[],
   classifiers=[
